b6c05c57c2b35c66bbf5b5f085cb5434d101ebf4
valerazubkov <valerazubkov7@gmail.com>
2023-03-09 21:57:18 +0300

    ее

--- index.js.js
+++ index.js.js
@@ -1,26 +1,10 @@
-const myCiti = {
-    citi: 'abc',
-    citiGriting() {
-        console.log('Gritings')
-    }
-}
-myCiti.citiGriting()
 
-// citigriting - метод со значением функцией и сперва свойсво объекта майСити
-// внутри функции вызов метода Лог объекта консоль(вывод в консоль строки)
-// в итоге вызываем метод Ситигритинг
-// без слова функция сокращенный формат
-const person  = {
-    name: 'Bob',
-    age: 21
-}
-const person2 = person
+function myFn() {}
 
-person2.age = 23
-person2.isAdult = true
+myFn()
 
-console.log(person.age)
-console.log(person.isAdult)
-// две переменные ссылаются на один объект в памяти
-// в переменных хранятся ссылки на объект
-// мутирование копий
+const myB = myFn()
+
+console.log (myB)
+
+// самая короткая функция
