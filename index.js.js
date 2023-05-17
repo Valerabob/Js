fa5b09c3eb40279d243984db94e9021e0a55434e
valerazubkov <valerazubkov7@gmail.com>
2023-05-17 20:09:26 +0300

    promise

--- index.js.js
+++ index.js.js
@@ -1,16 +1,26 @@
 
+const timerPromis = () =>
+    new Promise((resolve, reject) =>
+        setTimeout(() => resolve(),2000))
+
 const asyncFn = async () => {
-    return 'Success'
-}
+    console.log('Timer starts')
+    const startTime = performance.now()
+    await timerPromis()
+    const endTime = performance.now()
+    console.log('Timer ended', endTime -startTime )
+}       
 
 asyncFn()
-.then(value => console.log(value))
 
-// если же мы не просто вызовем фу-ию а еще и добавим вызов метода .then и в вызов этого метода
-// мы передадим колбэк фу-ию,то значением пере-ой value будет строка 'Success'
-// которую мы вернули из асинхронной фу-ии
+// в этом примере мы можеи отследить что мы ожидаем выполнение промиса
+// с помощью метода performance.now() можно засечь время,посчитать какой то интервал времени
+// Здесь создается пере-ая startTime она будет результатом вызова метода .now объекта performance
+// пере-ая endTime так же содержит результат вызова метода .now объекта performance
+// и далее после завершения этого промиса - await timerPromis() через 2сек
+// мы должны увидеть в консоле 'Timer ended' и разницу между выполнением команд, endTime-startTime в млс
 
-// const asyncFn = async () => {
- // return 'Success'
-// }                            в итоге здесь у нас не явно создается промис,без нашего участия
-//  а далее мы можем работать с этим промисом - добавлять вызов методов .then и .catch
+// еще раз: у нас фу-ия которая возв-ет промис -  timerPromis
+// и через 2сек промис станет исполненым,т.к. мы вызываем фу-ию resolve()
+// и задача данного примера что на этапе await timerPromis() мы ожидаем и не идем дальше
+
