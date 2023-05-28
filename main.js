705e0c83cfc06ce98eaab5517fdbf3d98b1d0bc5
valerazubkov <valerazubkov7@gmail.com>
2023-05-28 20:56:09 +0300

    yellow

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 
+const headings =  document.querySelectorAll('h1');
+console.log(headings)
 
-const autoBrands = ['Audi','Bmw','Mazda','Nissan']
+// Этой командой мы ищем коллекции элементов(все которые есть на странице h1
+// Но объект .classList - не работает для коллекций! только для одиночных элементов!
 
-autoBrands.forEach((item,index) => {
-    console.log(item,index)
+for(let item of headings) {
+    item.classList.add('blue-text');
+}
+// Поэтому пере-ой headings присваиваются все h1
+// После мы обходим их все с помощью цикла for-of,где item - каждый отдельный h1
+// А потом каждому отдельному заголовку добовляем .classList. 
+// И далее класс из CSS ('blue-text')
+
+ const paragraphs = document.querySelectorAll('p');
+ for(let p of paragraphs) {
+    p.classList.add('yellow-text');
+ }
+// Тот же пример,только с коллекцией P - обзацев!
+
+paragraphs.forEach (function (p) {
+    p.classList.add('yellow-text');
 })
 
-// В этом примере у нас перебор массива так же с помощью forEach,а функция переданна стрелочная => ! 
-// item - это значение элемента. index - это индекс эле-та!
-
-
-
-
-
-autoBrands.forEach((item,index) => console.log(item,index))
-
-// Сокращенное написание переборки массива,так как в фу-ии одна строчка !
+// Здесь мы обошли элементы (p) с помощью метода .forEach 
+// где (p) это каждый параграф. и далее мы уже добавили ему класс из CSS
