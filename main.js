a1ee44d5b78e5172e6a98733cab06e038559d500
valerazubkov <valerazubkov7@gmail.com>
2023-07-10 21:20:05 +0300

    uu

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
-//        --------/////////////   Цепочка промисов  /////////////-----------
-const checkRooms = new Promise (function(resolve,reject) {
-   setTimeout(function() {
-      console.log('Проверяем номера в отеле');
-      const availableRooms = true;
-      if (availableRooms) {
-         resolve('Номера есть');
-      }else{
-         reject('номеров нет');
-      }
-   },1500);
-});
-checkRooms.then(function(data) {
-  return new Promise (function(resolve,reject) {
-      setTimeout(() => {
-         console.log('then1')
-         console.log('Проверяем авиабилеты');
-         const availableTickets = true;
-            if (availableTickets) {
-               let message = 'Билеты есть';
-               resolve(message);
-            } else {
-               let message = 'Билетов нет'
-               reject(message);
-            }
+//        --------/////////////   Асинхронные фу-ии   asinc / await  /////////////-----------
+ function promiseFunction () {
+   return new Promise (function (resolve , reject) {
+      setTimeout (() => {
+         const result = true;
+
+         if (result) {
+            resolve('DONE');
+         } else {
+            reject('Fail');
+         }
       },1000);
-  });
-}).then (function(data){
-   console.log('then2');
-   console.log('Ура едем в отпуск');
-}).catch(function(data) {
-   console.log('ответ на предыдушем шаге-',data);
-   console.log('отпуск отменяется');
-});
+   });
+ }
+
+ async function startPromise () {
+   try  {
+      const result =  await promiseFunction ()
+      console.log (result)
+   } catch (err){
+      console.log(err)
+   }
+ }
+ startPromise()
+//  В примере мы потребляем промис с помощью asinc / await. Сначала фу-ий promiseFunction мы создали объект типа промис и сразу вернули его
+// Далее имитируем запрос,получаем ответ,записываем его в result. Далее условие.и на выходе мы получаем промис,а не данные из него
+// И что бы нам получить строку 'DONE'если будет resolve или 'Fail'если reject и с ней уже работать мы создаем асинхронную фу-ию startPromise
+//  где ждем с помощью await ждем выполнения промиса и его результат также записываем в пере-ую!
+// А используя блок try\catch по аналогии с методами .then и .cathc мы говорим что делать при успешном и не успешном выполнении промиса
+// При успешном -выполнится блок try - результат промиса запишется в конст и выведется в консоль строка 'DONE'.А при ошибки выполнится блок
+// catch и выведется в консоль 'Fail'. и эта ошибка будет поймана!
